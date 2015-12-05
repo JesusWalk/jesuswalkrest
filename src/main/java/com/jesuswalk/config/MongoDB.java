5690384b547f407e1b23344937fe95b825165edc
macv101 <michaelvillar0012@gmail.com>
2015-12-04 16:50:30 -0800

    Added credentials

--- src/main/java/com/jesuswalk/config/MongoDB.java
+++ src/main/java/com/jesuswalk/config/MongoDB.java
@@ -1,8 +1,7 @@
 package com.jesuswalk.config;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
-
-import javax.annotation.Resource;
 
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -10,18 +9,21 @@ import org.mongodb.morphia.Morphia;
 import com.jesuswalk.entity.BaseEntity;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 import com.mongodb.ReadPreference;
 import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 
-@Resource
 public class MongoDB {
 
 	public static final String DB_HOST = System.getenv("OPENSHIFT_MONGODB_HOST");
 	public static final int DB_PORT = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_PORT"));
-	public static final String DB_NAME = "jwtest";
+	public static final String DB_NAME = "jesuswalkrest";
 
+	public String DB_USER = "admin";
+	public String DB_PASSWORD = "zIRET8FIsNh3";
+	
 	private static final Logger LOG = Logger.getLogger(MongoDB.class.getName());
 
 	private static final MongoDB INSTANCE = new MongoDB();
@@ -34,6 +36,7 @@ public class MongoDB {
 
 	private void InitializeMongo() {
 		try {
+		MongoCredential credential = MongoCredential.createCredential(DB_USER, DB_NAME, DB_PASSWORD.toCharArray());
 		MongoClientOptions mongoOptions = MongoClientOptions.builder()
 					.socketTimeout(60000) // Wait 1m for a query to finish, https://jira.mongodb.org/browse/JAVA-1076
 					.connectTimeout(15000) // Try the initial connection for 15s, http://blog.mongolab.com/2013/10/do-you-want-a-timeout/
@@ -41,7 +44,7 @@ public class MongoDB {
 					.readPreference(ReadPreference.primaryPreferred()) // Read from the primary, if not available use a secondary
 					.build();
 		  MongoClient mongoClient;
-		  mongoClient = new MongoClient(new ServerAddress(DB_HOST, DB_PORT), mongoOptions);
+		  mongoClient = new MongoClient(new ServerAddress(DB_HOST, DB_PORT), Arrays.asList(credential), mongoOptions );
 		  mongoClient.setWriteConcern(WriteConcern.SAFE);
 		  datastore = new Morphia()
 				  .mapPackage(BaseEntity.class.getPackage().getName())

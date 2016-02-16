89b9d91a0aeab142b2a9f6a909dd585c214b2ad1
Manami Okada <mokada@cpp.edu>
2016-02-15 21:47:31 -0800

    Added a test service for email

--- src/main/java/com/jesuswalk/service/AppService.java
+++ src/main/java/com/jesuswalk/service/AppService.java
@@ -1,5 +1,12 @@
 package com.jesuswalk.service;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -21,12 +28,35 @@ public class AppService {
 	@Path("/email")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response register(Email email) {
-		String message = "";
-		message = email.getName() + " wants to know more about JesusWalk\n";
-		message += "Email: " + email.getEmailFrom();
-		message += "Message: " 	 + email.getMessage();		
-
+	public Response register(Email email) {	
+		
+		Email email2 = new Email();
+		email2.setEmailFrom("testsendfrom@whatever.com");
+		email2.setMessage("Hello test email");
+		email2.setName("Test Person");
+		email2.setSubject("Test subject");
+		
+		String mailTo = "mokada@jesuswalkyouth.com";
+		String mailFrom = email2.getEmailFrom();
+		String mailSubject = email2.getSubject();
+		String mailContent = email2.getMessage();
+		
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.host", "mail.jesuswalkyouth.com");
+			Session emailSession = Session.getDefaultInstance(properties);
+			
+			Message emailMessage = new MimeMessage(emailSession);
+			emailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
+			emailMessage.setFrom(new InternetAddress(mailFrom));
+			emailMessage.setSubject(mailSubject);
+			emailMessage.setText(mailContent);
+			
+			emailSession.setDebug(true);
+			Transport.send(emailMessage);
+		} catch (Exception e) {
+			return Response.status(Response.Status.EXPECTATION_FAILED).build();
+		}
 		
 	return Response.status(Response.Status.OK).build();
 		
